6144e2e6717a36222b823c7cc6b18af7e36385ed
hakzzang <mbh930312@naver.com>
2026-01-04 22:46:18 +0900

    디자인 프로토타입 xml 추가

--- app/src/main/java/com/the/snowball/MainActivity.kt
+++ app/src/main/java/com/the/snowball/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.the.snowball.ui.theme.TheSnowballTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheSnowballTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    AndroidView(
+                        factory = { context ->
+                            layoutInflater.inflate(R.layout.screen_home, null)
+                        }
                     )
                 }
             }
